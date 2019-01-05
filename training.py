977a273e7a6589a1da7976068592b4af7456c76e
stefanhartescu <46399361+stefanhartescu@users.noreply.github.com>
2019-01-05 14:06:04 +0000

    Add files via upload

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 
 train_X = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2])
 test_X = x_test.reshape(x_test.shape[0],x_test.shape[1]*x_test.shape[2])
+#reshape the data
 
 def SVM(trainX, trainY, testX):
     from sklearn import svm
@@ -12,12 +13,14 @@ def SVM(trainX, trainY, testX):
         tol=0.001, verbose=False)
     print(clf.predict(testX))
     return np.ravel(clf.predict(testX))
+	#trains an SVM with the obtained landmarks from the pictures
 
 predict_Y = SVM(train_X,y_train,test_X)
 size = len(predict_Y)
 
 def precisioncalculator(resultdata,testdata,sizedata):
     np.ravel(testdata)
+	# ravel used to save both vectors as line vectors
     counter = 0
     for x in range (0, sizedata - 1):
         if resultdata[x] == testdata[x]:
@@ -25,3 +28,4 @@ def precisioncalculator(resultdata,testdata,sizedata):
     return counter/sizedata
 
 precision = precisioncalculator(predict_Y, y_test, size)*100
+#calculates percentage of matches between predicted set and test set
